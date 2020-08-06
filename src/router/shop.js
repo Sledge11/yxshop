b9c1568432996dc25852e8b8872d4f02852a35e5
Sledge11 <13133271929@163.com>
2020-08-06 08:23:51 +0800

    购物车

---
+++ src/router/shop.js
@@ -0,0 +1,76 @@
+//自定义新路由文件
+import Home from "@/views/home";
+import Index from "@/views/home/index";
+import Category from "@/views/home/category";
+import Cart from "@/views/home/cart";
+import Me from "@/views/home/me";
+
+import Search from '@/components/search/search';
+import Detail from '@/views/home/detail';
+
+import Login from '@/views/login/login'
+
+
+export default [
+    {
+        path: "/",
+        name: "/",
+        component: Home,
+        redirect: "/index",
+        children: [
+            {
+                path: "/category",
+                name: "category",
+                component: Category,
+                meta: {
+                    title: "严选商城-分类"
+                }
+            }, {
+                path: "/index",
+                name: "index",
+                component: Index,
+                meta: {
+                    title: "严选商城-首页"
+                }
+            }, {
+                path: "/cart",
+                name: "cart",
+                component: Cart,
+                meta: {
+                    title: "严选商城-购物车"
+                }
+            }, {
+                path: "/me",
+                name: "me",
+                component: Me,
+                meta: {
+                    title: "严选商城-个人中心"
+                }
+            }
+        ]
+    },
+    {
+        path: "/search",
+        name: "Search",
+        component: Search,
+        meta: {
+            title: "严选商城-搜索"
+        }
+    },
+    {
+        path: "/detail/:id",
+        name: "detail",
+        component: Detail,
+        meta: {
+            title: "严选商城-商品详情"
+        }
+    },
+    {
+        path: "/login/",
+        name: "login",
+        component: Login,
+        meta: {
+            title: "用户登录"
+        }
+    },
+];
